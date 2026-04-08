3616716e162ca0fd0c5f7d5a8999c9e14eae1895
justinlai2325 <justinlai@sekolahbim.sch.id>
2026-04-08 10:56:23 +0700

    Add files via upload

---
+++ sumfor.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>
+ Formatif
+</title>
+ <script src="sumfor.js"></script>
+	<link rel="stylesheet" style="text/css" href ="sumfor.css">
+</head>
+<body>
+<button type="clear" onclick="clear"></button><br>
+<button type="ok" onclick=""> </button></br>//alert("");
+</div>
+</body>
+</html>
+
